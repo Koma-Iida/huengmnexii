2deabc7d88b22ef11fd408f98cde719ee9514a7b
Koma-Iida <iida.koma.x5@elms.hokudai.ac.jp>
2026-04-20 10:32:36 +0900

    after class

--- sec02/api/schemas/message.py
+++ sec02/api/schemas/message.py
@@ -9,7 +9,16 @@ class MessageBase(BaseModel):
     message: str | None = Field(None,
                                 examples=["Default Message"],
                                 description="Message body")
+    priority: int = Field(0, examples=[5],
+                          description="Message priority. "
+                          "Higher value means high priority.")
+    the_number_of_pizza_we_made_yesterday: int = Field(0,
+                                                              examples=[10],
+                                                              description='This is the number of pizza that we made yesterday.')
+    
 
 
 class Message(MessageBase):
-    time: datetime | None = Field(None, description="Message post time")
+    time: datetime | None = Field(None, 
+                                  description="Message post time")
+    
